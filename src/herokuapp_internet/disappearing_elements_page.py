70750c73253ef50f5dc362a99d89d0bf10a76725
ADAM MAGNESON <jamagneson@gmail.com>
2021-07-30 09:21:16 -0600

    Renamed files and added pytest fixtures

--- src/herokuapp_internet/disappearing_elements_page.py
+++ src/herokuapp_internet/disappearing_elements_page.py
@@ -4,7 +4,10 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-class DisappearingElements(Browser):
+class DisappearingElementsPage(Browser):
     def __init__(self, **kwargs):
         self._url = "http://the-internet.herokuapp.com/disappearing_elements"
         super().__init__(**kwargs)
+        self.get_page(self._url)
+
+    
--- src/herokuapp_internet/login_page.py
+++ src/herokuapp_internet/login_page.py
@@ -1,11 +1,10 @@
 from .browser import Browser
-from selenium.webdriver.common.keys import Keys
 import logging
 
 logger = logging.getLogger(__name__)
 
 
-class Login(Browser):
+class LoginPage(Browser):
     def __init__(self, **kwargs):
         self._url = "http://the-internet.herokuapp.com/login"
         super().__init__(**kwargs)
@@ -15,6 +14,7 @@ class Login(Browser):
         """Clear and fill the username field"""
         username_elem = self.driver.find_element_by_id("username")
         username_elem.clear()
+        logger.debug(f"Sending {username} to username input field")
         username_elem.send_keys(username)
         return self
 
@@ -22,15 +22,20 @@ class Login(Browser):
         """Clear and fill the password field"""
         password_elem = self.driver.find_element_by_id("password")
         password_elem.clear()
+        logger.debug(f"Sending {password} to password input field")
         password_elem.send_keys(password)
         return self
 
     def click_login_button(self):
         """Click on login button"""
+        logger.debug("Clicking login button")
         self.driver.find_element_by_css_selector(".radius").click()
 
     def get_error_message(self):
         """Get error message on failure"""
         error_elem = self.driver.find_element_by_id("flash")
         if error_elem.is_displayed():
+            logger.debug(f"Error message displayed: {error_elem.text}")
             return error_elem.text
+        logger.debug("No error displayed")
+        return None
