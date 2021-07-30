ca22df9bc17b4b782c6e7c3ef1390bcedf3ef288
ADAM MAGNESON <jamagneson@gmail.com>
2021-07-30 02:06:12 -0600

    Created method stubs and added init get page

--- src/herokuapp_internet/login.py
+++ src/herokuapp_internet/login.py
@@ -8,3 +8,16 @@ class Login(Browser):
     def __init__(self, **kwargs):
         self._url = "http://the-internet.herokuapp.com/login"
         super().__init__(**kwargs)
+        self.get_page(self._url)
+
+    def fill_username(self, username):
+        pass
+
+    def fill_password(self, password):
+        pass
+
+    def click_login_button(self):
+        pass
+
+    def get_error_message(self):
+        pass
