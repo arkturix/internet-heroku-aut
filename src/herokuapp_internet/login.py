c2752c168ad0961a9d2eb71d01a1496db3f3d06e
ADAM MAGNESON <jamagneson@gmail.com>
2021-07-30 02:28:21 -0600

    Filled out methods for login page object

--- src/herokuapp_internet/login.py
+++ src/herokuapp_internet/login.py
@@ -1,4 +1,5 @@
 from .browser import Browser
+from selenium.webdriver.common.keys import Keys
 import logging
 
 logger = logging.getLogger(__name__)
@@ -10,14 +11,26 @@ class Login(Browser):
         super().__init__(**kwargs)
         self.get_page(self._url)
 
-    def fill_username(self, username):
-        pass
+    def fill_username(self, username: str):
+        """Clear and fill the username field"""
+        username_elem = self.driver.find_element_by_id("username")
+        username_elem.clear()
+        username_elem.send_keys(username)
+        return self
 
-    def fill_password(self, password):
-        pass
+    def fill_password(self, password: str):
+        """Clear and fill the password field"""
+        password_elem = self.driver.find_element_by_id("password")
+        password_elem.clear()
+        password_elem.send_keys(password)
+        return self
 
     def click_login_button(self):
-        pass
+        """Click on login button"""
+        self.driver.find_element_by_css_selector(".radius").click()
 
     def get_error_message(self):
-        pass
+        """Get error message on failure"""
+        error_elem = self.driver.find_element_by_id("flash")
+        if error_elem.is_displayed():
+            return error_elem.text
