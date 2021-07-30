5415105d943cb572d13133edc4ccd335833d1032
ADAM MAGNESON <jamagneson@gmail.com>
2021-07-30 10:53:30 -0600

    Added methods to login page

--- src/herokuapp_internet/login_page.py
+++ src/herokuapp_internet/login_page.py
@@ -10,6 +10,11 @@ class LoginPage(Browser):
         super().__init__(**kwargs)
         self.get_page(self._url)
 
+    def action_is_successful(self) -> bool:
+        """Determine if an action is succesfful based on presence of success banner"""
+        success_banner_elem = self.driver.find_element_by_css_selector("#flash.flash.success")
+        return success_banner_elem.is_displayed()
+
     def fill_username(self, username: str):
         """Clear and fill the username field"""
         username_elem = self.driver.find_element_by_id("username")
@@ -31,11 +36,31 @@ class LoginPage(Browser):
         logger.debug("Clicking login button")
         self.driver.find_element_by_css_selector(".radius").click()
 
+    def login_success(self):
+        """Determine if login is successful"""
+        return self.action_is_successful()
+
+    def login_success_message(self):
+        success_banner_elem = self.driver.find_element_by_css_selector("#flash.flash.success")
+        if success_banner_elem.is_displayed():
+            logger.debug(f"Success message displayed: {success_banner_elem.text}")
+            return success_banner_elem.text
+        return None
+
     def get_error_message(self):
         """Get error message on failure"""
-        error_elem = self.driver.find_element_by_id("flash")
-        if error_elem.is_displayed():
-            logger.debug(f"Error message displayed: {error_elem.text}")
-            return error_elem.text
+        error_banner_elem = self.driver.find_element_by_id("#flash.flash.error")
+        if error_banner_elem.is_displayed():
+            logger.debug(f"Error message displayed: {error_banner_elem.text}")
+            return error_banner_elem.text
         logger.debug("No error displayed")
         return None
+
+    def click_logout_button(self):
+        """Click on the logout button"""
+        logger.debug("Clicking the logout button")
+        self.driver.find_element_by_css_selector('a.button.secondary.radius').click()
+
+    def logout_success(self):
+        """Determine if logout is successful"""
+        return self.action_is_successful()
